f1d05bbac94c11226d841e2549f5292b7fcd629c
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2019-03-03 18:51:35 +0300

    First Commit

---
+++ JWT/Controllers/TokenController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration configuration;
+
+        public TokenController(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        [HttpGet("CreateToken")]
+        public IActionResult Index(string username = "faruk", string password = "faruk")
+        {
+            IActionResult response  = Unauthorized();
+            if (username.Equals(password))
+            {
+                var token = JwtTokenBuilder();
+
+                response = Ok(new { access_token = token });
+            }
+            return response;
+        }
+
+        private string JwtTokenBuilder()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                configuration["JWT:key"]
+                ));
+
+            var credintials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var jwtToken = new JwtSecurityToken(issuer: configuration["JWT:issuer"],
+                audience: configuration["JWT:audience"],
+                signingCredentials: credintials,
+                expires: DateTime.Now.AddDays(30),
+                claims: new List<Claim>() {new Claim("name","faruk"),new Claim("system_role","admin") }
+                );           
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtToken);
+        }
+    }
+}
---
+++ JWT/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Text;
+
+namespace JWT
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Token Parameter Required
+            var tokenparams = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidIssuer = Configuration["JWT:issuer"],
+                ValidAudience = Configuration["JWT:audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:key"]))
+            };
+
+            //Token
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(jwtconfig =>
+                {
+                    jwtconfig.TokenValidationParameters = tokenparams;
+                });
+
+            //Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("CoreSwagger", new Info
+                {
+                    Title = "Swagger on ASP.NET Core",
+                    Version = "1.0.0",
+                    Description = "Try Swagger on (ASP.NET Core 2.1)",
+                    Contact = new Contact()
+                    {
+                        Name = "Swagger Implementation Faruk Şahin",
+                        Email = "faruk_thecno@hotmail.com"
+                    },
+                    TermsOfService = "http://swagger.io/terms/"
+                });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //Token
+            app.UseAuthentication();          
+            app.UseMvcWithDefaultRoute();
+
+            //Swagger
+            app.UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/CoreSwagger/swagger.json", "Swagger Test .Net Core");
+                
+            });
+
+        }
+    }
+}
